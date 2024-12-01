89e9a3cf2f17c3fe894ef9ecb53a7ec20b837667
akeeton <andrewrkeeton@gmail.com>
2024-12-01 15:41:17 -0500

    Add .vscode templates to project creation

--- create-fsharp-project.sh
+++ create-fsharp-project.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+
+# Fail on any error
+set -e
+
 if [[ $# -eq 0 ]] ; then
     echo 'usage: create-fsharp-project.sh <project-name>'
     exit 1
 fi
 
-dotnet new console -lang "F#" -o $1
+export PROJECT_NAME=$1
+PROJECT_PATH=$(realpath "$PROJECT_NAME")
+SUBST_VARS="\$PROJECT_NAME:" # Separate var names with colons
+
+dotnet new console -lang "F#" -o "$PROJECT_NAME"
+mkdir -p "$PROJECT_PATH/.vscode"
+
+pushd ".vscode-templates"
+for f in ./*; do
+    echo "$f"
+    envsubst "$SUBST_VARS" < "$f" > "$PROJECT_PATH/.vscode/$f"
+done
+popd
