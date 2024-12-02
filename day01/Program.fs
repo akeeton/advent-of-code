cad4e8684cf5492a4bd4d6b176bb118e67b0940a
akeeton <andrewrkeeton@gmail.com>
2024-12-01 19:42:28 -0500

    Solve day 1 part 2 and refactor

--- day01/Program.fs
+++ day01/Program.fs
@@ -1,17 +1,36 @@
 ﻿open System.IO
 
-module Tuple2 =
-    let map f (a, b) = (f a, f b)
+let partOne (sortedLeftNums: int list) (sortedRightNums: int list): int =
+    let folder (distance: int) (left: int) (right: int): int =
+        abs (left - right) + distance
 
-let splitIntoNumPair (s: string) =
+    List.fold2 folder 0 sortedLeftNums sortedRightNums
+
+let partTwo (sortedLeftNums: int list) (sortedRightNums: int list): int =
+    let rightNumsCounts =
+        sortedRightNums |> List.countBy id |> Map
+
+    let calculateSimilarity (leftNum: int): int =
+        match Map.tryFind leftNum rightNumsCounts with
+        | Some rightNumCount -> leftNum * rightNumCount
+        | None -> 0
+
+    sortedLeftNums
+    |> List.map calculateSimilarity
+    |> List.sum
+
+let splitInputLineNums (s: string): int * int =
     let numbers = s.Split "   " |> Array.map int
     numbers.[0], numbers.[1]
 
-File.ReadLines "input.txt"
-|> Seq.map splitIntoNumPair
-|> Seq.toList
-|> List.unzip
-|> Tuple2.map List.sort
-|> (fun (left, right) -> List.zip left right)
-|> List.fold (fun state (a, b) -> state + abs (a - b)) 0
-|> printfn "answer: %d"
+let mapTuple2 f (a, b) = (f a, f b)
+
+let sortedLeftNums, sortedRightNums =
+    File.ReadLines "input.txt"
+    |> Seq.map splitInputLineNums
+    |> Seq.toList
+    |> List.unzip
+    |> mapTuple2 List.sort
+
+printfn "Part one answer: %A" <| partOne sortedLeftNums sortedRightNums
+printfn "Part two answer: %A" <| partTwo sortedLeftNums sortedRightNums
