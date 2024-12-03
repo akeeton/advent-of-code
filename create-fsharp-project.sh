e09c57c8505b8d7dc16cc16dd7d3e19055b44a0f
akeeton <andrewrkeeton@gmail.com>
2024-12-03 14:32:14 -0500

    Handle multiple years in project creation script

--- create-fsharp-project.sh
+++ create-fsharp-project.sh
@@ -3,30 +3,37 @@
 # Fail on any error
 set -e
 
-if [[ $# -eq 0 ]] ; then
-    echo 'usage: create-fsharp-project.sh <project-name>'
+if [[ $# -lt 2 ]] ; then
+    echo 'usage: create-fsharp-project.sh <year> <project-name>'
     exit 1
 fi
 
-export PROJECT_NAME=$1
-PROJECT_PATH=$(realpath "$PROJECT_NAME")
+YEAR=$1
+export PROJECT_NAME=$2
+
+mkdir -p "$YEAR"
+
+PROJECT_PATH=$(realpath "$YEAR/$PROJECT_NAME")
 SUBST_VARS="\$PROJECT_NAME:" # Separate var names with colons
 
-dotnet new console -lang "F#" -o "$PROJECT_NAME"
-mkdir -p "$PROJECT_PATH/.vscode"
 
+pushd "$YEAR"
+    dotnet new console -lang "F#" -o "$PROJECT_NAME"
+popd # "$YEAR"
+
+mkdir -p "$PROJECT_PATH/.vscode"
 pushd ".vscode-templates"
-for f in ./*; do
-    echo "Substituting environment variables in '$f'"
-    envsubst "$SUBST_VARS" < "$f" > "$PROJECT_PATH/.vscode/$f"
-done
-popd
+    for f in ./*; do
+        echo "Substituting environment variables in '$f'"
+        envsubst "$SUBST_VARS" < "$f" > "$PROJECT_PATH/.vscode/$f"
+    done
+popd # ".vscode-templates"
 
 pushd "$PROJECT_PATH"
-for f in ./*; do
-    if [ -f "$f" ]; then
-        echo "Removing BOM from '$f'"
-        dos2unix -r "$f"
-    fi
-done
-popd
+    for f in ./*; do
+        if [ -f "$f" ]; then
+            echo "Removing BOM from '$f'"
+            dos2unix -r "$f"
+        fi
+    done
+popd # "$PROJECT_PATH"
