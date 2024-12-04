166522071344e29f00199986c8bd4d2f63013540
akeeton <andrewrkeeton@gmail.com>
2024-12-04 07:17:17 -0500

    Create 2024 day 4 solution

---
+++ 2024/day04/Service/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace Service
+
+type Matrix<'T> = 'T[,]
+module Matrix =
+    let fromListOfRows (rows: 'T seq seq): Matrix<'T> =
+        array2D rows
+
+    let rows (matrix: Matrix<'T>): 'T seq =
+        failwith "unfinished"
